02ff9474d46feeafe5dedc0f0dd42b615e75841c
SamuelSNTS04 <malakaigamer5@gmail.com>
2025-09-26 00:32:29 -0300

    Desafio de projeto

---
+++ desafio-de-projeto/index.js
@@ -0,0 +1,23 @@
+let nome = 'Samuel';
+let quantidadeDeExperiencia = 2000;
+let nivel;
+
+if (quantidadeDeExperiencia < 1000) {
+    nivel = 'Ferro';
+} else if (quantidadeDeExperiencia >= 1001 || quantidadeDeExperiencia <= 2000) {
+    nivel = 'Bronze';
+} else if (quantidadeDeExperiencia >= 2001 || quantidadeDeExperiencia <= 5000) {
+    nivel = 'Prata';
+} else if (quantidadeDeExperiencia >= 5001 || quantidadeDeExperiencia <= 7000) {
+    nivel = 'Ouro';
+} else if (quantidadeDeExperiencia >= 7001 || quantidadeDeExperiencia <= 8000) {
+    nivel = 'Platina';
+} else if (quantidadeDeExperiencia >= 8001 || quantidadeDeExperiencia <= 9000) {
+    nivel = 'Ascendente';
+} else if (quantidadeDeExperiencia >= 9001 || quantidadeDeExperiencia <= 10000) {
+    nivel = 'Imortal';
+} else if (quantidadeDeExperiencia >= 10001) {
+    nivel = 'Radiante';
+}
+
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
